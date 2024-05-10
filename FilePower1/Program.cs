921a4cffca079acf9ec8b576219dbdaa1031aa16
Gabriela Branco <lindosogb@gmail.com>
2024-05-10 14:14:10 +0100

    Added program that accepts a txt file and writes content inside

--- FilePower1/Program.cs
+++ FilePower1/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+
 
 namespace FilePower1
 {
     class Program
     {
-        public static void Main()
+        public static void Main(string[] file)
         {
-            
+            string path = Path.Combine("FilePower1", file[0]);
+            string contents = "";
+
+            Queue<string> queue = new Queue<string>();
+            string line = null;
+
+            while ((line = Console.ReadLine()) != "")
+            {
+                queue.Enqueue(line);
+            }
+
+            foreach (string element in queue)
+            {
+                contents += $"{element}\n";
+            }
+
+            File.WriteAllText(path, contents);
         }
     }
 }
