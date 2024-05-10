b4463f9908e51d0c2baba8e5c59b0b0f36ba477c
Gabriela Branco <lindosogb@gmail.com>
2024-05-10 14:32:11 +0100

    Created FilePower2, that dismiss the use of a queue

---
+++ FilePower2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FilePower2
+{
+    class Program
+    {
+        public static void Main(string[] fileName)
+        {
+            string content = null;
+
+            using StreamWriter sw = new StreamWriter("FilePower2/"+fileName[0]);
+
+            while ((content = Console.ReadLine()) != "")
+            {
+                Console.WriteLine(content);
+                sw.WriteLine(content);
+            }
+        }
+    }
+}
