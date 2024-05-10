5860d2e2bd55f72d53b66435ceed917518cfb4e9
Gabriela Branco <lindosogb@gmail.com>
2024-05-10 15:51:13 +0100

    Created program that reads files and returns commom chars inside them, in order

--- CharSets/Program.cs
+++ CharSets/Program.cs
@@ -8,11 +8,51 @@ namespace CharSets
     {
         static void Main(string[] files)
         {
-            List<char>[] filesList = new List<char>[files.Length];
-            for(int i=0; i < files.Length; i++)
-            {
-                StreamReader sr = new StreamReader(files[i]);
+            HashSet<char>[] filesList = new HashSet<char>[files.Length];
+            string s = "";
 
+            for(int i=0; i < filesList.Length; i++)
+            {
+                filesList[i] = new HashSet<char>();
+            }
+
+            for(int i=0; i < filesList.Length; i++)
+            {
+                string path = Path.Combine("CharSets", files[i]);
+                using StreamReader sr = new StreamReader(path);
+
+                while (( s = sr.ReadLine()) != null )
+                {
+                    if( s.Length > 1)
+                    {
+                        Console.WriteLine("Error");
+                        return;
+                    }
+                    else
+                    {
+                        filesList[i].Add(char.Parse(s));
+                    }
+                    
+                }
+            }
+
+            foreach (HashSet<char> chars in filesList)
+            {
+                filesList[0].IntersectWith(chars);
+            }
+
+            List<char> finalList = new List<char>();
+
+            foreach(char c in filesList[0])
+            {
+                finalList.Add(c);
+            }
+
+            finalList.Sort();
+
+            foreach(char c in finalList)
+            {
+                Console.WriteLine(c);
             }
         }
     }
